2ef721192e1dd4054121499fd5fc536de4e6e20f
Jackson McCrory <jackson@mccrory.xyz>
2021-10-25 22:12:11 -0700

    new initial commit

--- config.h
+++ config.h
@@ -3,19 +3,19 @@
 
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-    "Sauce Code Pro Nerd Font:size=10",
-    "JoyPixels:size=10:antialias=true:autohint=true"};
+static const char *fonts[] = {"Iosevka Nerd Font:size=11",
+                              "JoyPixels:size=10:antialias=true:autohint=true"};
 static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
-    [SchemeNorm] = {"#bbbbbb", "#222222"},
-    [SchemeSel] = {"#eeeeee", "#005577"},
+    [SchemeNorm] = {"#ffffff", "#1e1e1e"},
+    [SchemeSel] = {"#344EB2", "#1e1e1e"},
     [SchemeOut] = {"#000000", "#00ffff"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 0;
+static int lineheight = 20;
 
 /*
  * Characters not considered part of a word while deleting words
